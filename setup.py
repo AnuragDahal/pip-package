d163060972cf3895e64940e8304b8fed8e2c908d
AnuragDahal <freefirepros2020@gmail.com>
2024-07-19 08:04:33 +0545

    chore: Bump version number in setup.py to 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi-mongo',
-    version='0.0.5',
+    version='0.0.6',
     long_description='This is a simple package that helps you to create a FastAPI project with MongoDB it comes with default crud operations and database setup just configure your environment variables and you are good to go.',
     classifiers=[
         'Development Status :: 3 - Alpha',
