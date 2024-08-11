6f5518ee955e35284207bd937cca5568fe903a0f
AnuragDahal <freefirepros2020@gmail.com>
2024-08-11 13:37:14 +0545

    chore: Update version number in setup.py to 0.0.8 fixed minor bugs

--- package/fastapi-mongo/app/handlers/User/userhandler.py
+++ package/fastapi-mongo/app/handlers/User/userhandler.py
@@ -1,9 +1,7 @@
 from ...models import schemas
-from fastapi import Response
 from ...core.database import user_collection
 from ..exception import ErrorHandler
 from ...utils.passhashutils import Encrypt
-from ...core.database import post_collection, comments_collection
 from ...utils.passhashutils import Encrypt
 from ..Auth.authhandler import Validate
 
@@ -41,23 +39,4 @@ class UserManager:
             return users
         else:
             raise ErrorHandler.NotFound("No user found")
-
-    @staticmethod
-    async def HandleUserDeletion(user_email: str, password: str, res: Response):
-        """
-        Delete a user
-        """
-        # Check the password is correct
-        user_details = await user_collection.find_one({"email": user_email})
-        if not Encrypt.verify_password(password, user_details["password"]):
-            return ErrorHandler.Unauthorized("Incorrect Password")
-
-        # Delete the user through the email
-        deleted_user = await user_collection.delete_one({"email": user_email})
-        if deleted_user.deleted_count == 0:
-            return ErrorHandler.NotFound("User not found")
-        # Delete all the data sets associated with the user such as posts, comments, etc.
-        await comments_collection.delete_many({"commented_by": user_email})
-        await post_collection.delete_many({"posted_by": user_email})
-        res.delete_cookie('token')
-        return {"message": "User deleted successfully"}
+        
